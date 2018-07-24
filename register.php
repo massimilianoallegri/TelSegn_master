347c8eb7f18adfbf553d5493bfbf1430bd3cd2cf
massimilianoallegri <massimiliano.allegri@gmail.com>
2018-07-24 14:58:52 +0200

    register

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://creasegnalazione.herokuapp.com/execute.php';
+$BOT_TOKEN = '332192248:AAGzQJsHm7GcHUmX8zlNNCIBfrSC6iHFoOQ';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
